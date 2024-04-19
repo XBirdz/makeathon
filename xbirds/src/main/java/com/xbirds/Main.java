a9474779204ad36f26800ebd8d91425e9e1f3f95
tasosger <tassos.gerassis@gmail.com>
2024-04-20 00:56:23 +0300

    kdsdvcsv

--- xbirds/src/main/java/com/xbirds/Main.java
+++ xbirds/src/main/java/com/xbirds/Main.java
@@ -3,5 +3,6 @@ package com.xbirds;
 public class Main {
     public static void main(String args[]){
         System.out.println(TravelmythAPI.apireq("Athens", "ski"));
+        System.out.println(UnsplashAPIclient.jsonextract(UnsplashAPIclient.fetchPhotos("sunny")));
     }
 }
