3dde91adc9e924e0d6a0d845b2d6a7b88ecf7122
tasosger <tassos.gerassis@gmail.com>
2024-04-20 10:55:56 +0300

    fixed gpt prompt

--- xbirds/src/main/java/com/xbirds/GptConnector.java
+++ xbirds/src/main/java/com/xbirds/GptConnector.java
@@ -5,7 +5,19 @@ public class GptConnector {
         OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\"model\": \"gpt-3.5-turbo-0125\", \"temperature\": 0, \"top_p\": 1, \"max_tokens\": 1200, \"n\": 1, \"messages\": [{\"role\": \"user\", \"content\": \"Hello, how are you?\"}]}");
+        String bodyString = "{" +
+        "\"model\": \"gpt-3.5-turbo-0125\", " +
+        "\"temperature\": 0, " +
+        "\"top_p\": 1, " +
+        "\"max_tokens\": 15000, " +
+        "\"n\": 1, " +
+        "\"messages\": [" +
+        "{\"role\": \"travel-assistant\", \"content\": \"You will provide travel destinations based on the requirements and desires of the user. You will provide a list " +
+        "with only the destination names. Nothing else. Approximately 10 destinations.This is the user desires: Hi i want a romantic beach destintion for a city break. I live in athens.\"}" +
+        "],";
+        RequestBody body = RequestBody.create(mediaType, "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"You will provide travel destinations based on the requirements and desires of the user. You will provide a list " +
+        "with only the destination names. Nothing else. Approximately 10 destinations.This is the user desires: Hi i want a romantic beach destintion for a city break. I live in athens.\"}], \"temperature\": 0.7}");
+        //RequestBody body = RequestBody.create(mediaType, bodyString);
 
         Request request = new Request.Builder()
           .url("https://api.openai.com/v1/chat/completions")
