7ae3fa7207f43a3c75610106afbeca8107996e72
crissel04 <t8220135@aueb.gr>
2024-04-20 03:18:48 +0300

    Create the basics of the start page

--- xbirds/src/main/java/com/xbirds/Main.java
+++ xbirds/src/main/java/com/xbirds/Main.java
@@ -2,8 +2,8 @@ package com.xbirds;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println(TravelmythAPI.apireq("Athens", "ski"));
-        System.out.println(UnsplashAPIclient.jsonextract(UnsplashAPIclient.fetchPhotos("winter")));
-        System.out.println(Weather.requestWeather(37.96932, 23.73524, ""));
+        // System.out.println(TravelmythAPI.apireq("Athens", "ski"));
+        // System.out.println(UnsplashAPIclient.jsonextract(UnsplashAPIclient.fetchPhotos("winter")));
+        // System.out.println(Weather.requestWeather(37.96932, 23.73524, ""));
     }
 }
--- xbirds/src/main/java/com/xbirds/controllers/StartPageController.java
+++ xbirds/src/main/java/com/xbirds/controllers/StartPageController.java
@@ -1,16 +1,27 @@
 package com.xbirds.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class StartPageController { 
         @FXML
-        private ImageView powerButton;
+        private TextField activityTextField;
+
+        @FXML
+        private TextField dateTextField;
+
+        @FXML
+        private TextField vacationDaysTextField;
+
+        @FXML
+        private TextField weatherTextField;
     
         @FXML
         void powerOff(MouseEvent event) {
                 System.out.println("Power Off presser");
+                System.exit(0);
         }
 }
 
