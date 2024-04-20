a89da89202a3287d5f5b0f036c10a0bd0da5bae4
tasosger <tassos.gerassis@gmail.com>
2024-04-20 11:36:07 +0300

    obtained city info

--- xbirds/src/main/java/com/xbirds/WeatherHandler.java
+++ xbirds/src/main/java/com/xbirds/WeatherHandler.java
@@ -3,8 +3,12 @@ import java.util.List;
 public class WeatherHandler {
     private static List<String> cities;
     private static String[] weathers;
+    private static String type;
     public static void init(String input){
         cities = GptConnector.handleUserInp(input);
+        for (String city : cities) {
+            TravelmythAPI.createInfo(city, type);
+        }
     }
     
 }
