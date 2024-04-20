205c80ee1aa511716669d5e2fc4615896afca88a
tasosger <tassos.gerassis@gmail.com>
2024-04-20 15:45:40 +0300

    weather info

--- xbirds/src/main/java/com/xbirds/WeatherInfo.java
+++ xbirds/src/main/java/com/xbirds/WeatherInfo.java
@@ -2,12 +2,17 @@ package com.xbirds;
 
 
 class WeatherInfo {
-    private double maxTemp;
-    private double minTemp;
+    private double avgmaxTemp;
+    private double avgminTemp;
+    private double totalmaxTemp;
+    private double totalminTemp;
+
     private int weatherCode;
-    public WeatherInfo(double m1,double m2, int c){
-        maxTemp= m1;
-        minTemp =m2;
+    public WeatherInfo(double m1,double m2,double m3,double m4, int c){
+        avgmaxTemp= m1;
+        avgminTemp =m2;
+        totalmaxTemp= m1;
+        totalminTemp =m2;
         weatherCode=c;
     }
     
