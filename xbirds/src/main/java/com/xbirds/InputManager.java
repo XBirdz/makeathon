5415dd1d9f552037311e5ccf0395ce2768d35bb5
tasosger <tassos.gerassis@gmail.com>
2024-04-20 15:59:43 +0300

    Input receiver

---
+++ xbirds/src/main/java/com/xbirds/InputManager.java
@@ -0,0 +1,9 @@
+package com.xbirds;
+
+public class InputManager {
+
+    public static String promptCreator(String activity, String weather){
+        String prompt = "Hi i want a" +activity+"destintion for a city break. I live in athens. My preferred weather is"+weather;
+        return prompt;
+    }
+}
