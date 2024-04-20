af529e6f4dc715dbda33c3f138fefb6e48562fa8
crissel04 <t8220135@aueb.gr>
2024-04-20 15:52:41 +0300

    Enhance StartPage

--- xbirds/src/main/java/com/xbirds/controllers/StartPageController.java
+++ xbirds/src/main/java/com/xbirds/controllers/StartPageController.java
@@ -2,7 +2,10 @@ package com.xbirds.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class StartPageController { 
         @FXML
@@ -16,6 +19,20 @@ public class StartPageController {
 
         @FXML
         private TextField weatherTextField;
+
+        @FXML
+        private VBox inputBox;
+
+        @FXML
+        private ProgressBar progressBar;
+
+        @FXML
+        private Text stepText;
+
+        @FXML
+        void submitInfo() {
+
+        }
     
         @FXML
         void powerOff(ActionEvent event) {
