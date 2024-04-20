7f5f1f69f6935487327e8eafa6776a36ddaadf26
Stefanos Kampouris <stevekamp03@gmail.com>
2024-04-20 18:58:00 +0300

    updated Datelisteners

--- xbirds/src/main/java/com/xbirds/controllers/StartPageController.java
+++ xbirds/src/main/java/com/xbirds/controllers/StartPageController.java
@@ -115,6 +115,7 @@ public class StartPageController {
             minPriceListener();
             maxPriceListener();
 
+
         }
 
         private void showAlert(String title, String message) {
