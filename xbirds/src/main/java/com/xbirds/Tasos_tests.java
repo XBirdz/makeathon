6218b821314566c62f5db4f4ffbde8e1b6ec71ea
tasosger <tassos.gerassis@gmail.com>
2024-04-20 12:46:35 +0300

    Tasos_tests

---
+++ xbirds/src/main/java/com/xbirds/Tasos_tests.java
@@ -0,0 +1,9 @@
+package com.xbirds;
+
+public class Tasos_tests {
+    public static void test(){
+        WeatherHandler w = new WeatherHandler();
+        w.init("sdkjvcns", "ski");
+       
+    }
+}
