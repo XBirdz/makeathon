bb58953dbb807e133979740ed65dee5e7a1543e3
tasosger <tassos.gerassis@gmail.com>
2024-04-20 14:03:46 +0300

    info

---
+++ xbirds/src/main/java/com/xbirds/WeatherInfo.java
@@ -0,0 +1,14 @@
+package com.xbirds;
+
+
+class WeatherInfo {
+    private double maxTemp;
+    private double minTemp;
+    private int weatherCode;
+    public WeatherInfo(double m1,double m2, int c){
+        maxTemp= m1;
+        minTemp =m2;
+        weatherCode=c;
+    }
+    
+}
