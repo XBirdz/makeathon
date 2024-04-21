2eca883924554dd5b51d4e42b27a48eb6a4583ba
tasosger <tassos.gerassis@gmail.com>
2024-04-21 11:06:17 +0300

    djdsd

--- xbirds/src/main/java/com/xbirds/Tasos_tests.java
+++ xbirds/src/main/java/com/xbirds/Tasos_tests.java
@@ -2,11 +2,7 @@ package com.xbirds;
 import java.util.*;
 public class Tasos_tests {
     public static void test(){
-        WeatherHandler w = new WeatherHandler();
-        List<String> features = new ArrayList<>();
-        features.add("pool");
-       
-        w.init( features,"ski", "snowy,no rain","far","Athens","cheap","couple","winter");
+        
        //Embeddings.encode("null", features);
        //Embeddings embeddings = new Embeddings();
        //List<Integer> closestIndices = embeddings.findClosestPhraseIndices("water",features);
