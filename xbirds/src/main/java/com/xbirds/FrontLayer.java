5efd7fd85250d15424b2d5ab25ccfc33f6b8b8ff
tasosger <tassos.gerassis@gmail.com>
2024-04-21 10:15:29 +0300

    front layer

--- xbirds/src/main/java/com/xbirds/FrontLayer.java
+++ xbirds/src/main/java/com/xbirds/FrontLayer.java
@@ -1,8 +1,9 @@
 package com.xbirds;
 import java.util.*;
+import javafx.util.Pair;
 public class FrontLayer {
-    public static void giveUserData(List<String> features,String type, String weatherInp, String distance, String location, String budget, String company, String season){
+    public static List<Pair<CityInfo,WeatherInfo>> giveUserData(List<String> features,String type, String weatherInp, String distance, String location, String budget, String company, String season,String checkinDay, String checkinMonth, String checkinYear, String checkoutDay, String checkoutMonth, String checkoutYear){
         WeatherHandler w = new WeatherHandler();
-        w.init(features, type, weatherInp, distance, location, budget, company, season);
+       return w.init(features, type, weatherInp, distance, location, budget, company, season,checkinDay, checkinMonth, checkinYear, checkoutDay, checkoutMonth, checkoutYear);
     } 
 }
